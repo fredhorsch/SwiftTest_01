0e5cbfd5196ac47c86b0f0fde32d02c923ed1996
Frederic Horsch <frederichorsch@Frederics-MacBook-Pro.local>
2020-09-01 15:40:10 +0200

    Added Calc Keypad

--- Test_01/CalculatorView.swift
+++ Test_01/CalculatorView.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct CalculatorView: View {
+    
+    @State var calcNumbers = ""
+    
     var body: some View {
-        Text("")
+        
+        VStack {
+            
+            Text(calcNumbers)
+            
+            HStack {
+                ButtonDesign(number: 1, calcNum: $calcNumbers)
+                ButtonDesign(number: 2, calcNum: $calcNumbers)
+                ButtonDesign(number: 3, calcNum: $calcNumbers)
+            }
+            
+            HStack {
+                ButtonDesign(number: 4, calcNum: $calcNumbers)
+                ButtonDesign(number: 5, calcNum: $calcNumbers)
+                ButtonDesign(number: 6, calcNum: $calcNumbers)
+            }
+            
+            HStack {
+                ButtonDesign(number: 7, calcNum: $calcNumbers)
+                ButtonDesign(number: 8, calcNum: $calcNumbers)
+                ButtonDesign(number: 9, calcNum: $calcNumbers)
+            }
+        }
+    }
+    
+}
+
+struct ButtonDesign: View {
+    var number:Int
+    
+    @Binding var calcNum: String
+    
+    var body: some View {
+        
+        Button("\(number)") {
+            calcNum = calcNum + "\(number)"
+        }.font(.largeTitle).padding(20)
+        
     }
     
 }
